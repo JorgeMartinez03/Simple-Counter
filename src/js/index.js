5404528f10493d5fb00eb91fa759c96a40131a9d
JorgeMartinez03 <94927018+JorgeMartinez03@users.noreply.github.com>
2024-03-12 16:55:47 +0000

    Finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,26 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
+import SecondsCounter from "../js/component/counter";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+
+setInterval(() => {
+
+    let six = Math.floor((counter / 100000) % 10);
+    let five = Math.floor((counter / 10000) % 10);
+    let four = Math.floor((counter / 1000) % 10);
+    let three = Math.floor((counter / 100) % 10);
+    let two = Math.floor((counter / 10) % 10);
+    let one = counter % 10;
+
+    counter++
+
+    console.log("Este es el contador de cada digito!!!! ----->", six, five, four, three, two, one)
+
+    ReactDOM.render(
+        <SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} />,
+        document.querySelector("#app")
+    );
+}, 1000);
